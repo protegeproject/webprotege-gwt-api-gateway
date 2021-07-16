d073efd69982736cd19ec340e82e480ed346a736
Matthew Horridge <matthew.horridge@stanford.edu>
2021-07-16 16:31:09 -0700

    Initial commit

---
+++ src/main/java/edu/stanford/protege/webprotege/gateway/WebprotegeGwtApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package edu.stanford.protege.webprotege.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprotegeGwtApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebprotegeGwtApiGatewayApplication.class, args);
+	}
+
+}
