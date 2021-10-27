59e3967b98d962bdec4c34aa6fc091347734f373
Matthew Horridge <matthew.horridge@stanford.edu>
2021-10-27 16:26:45 -0700

    Changed to resource server (from client).  Obtaining a JWT is done elsewhere

--- src/main/java/edu/stanford/protege/webprotege/gateway/GatewayController.java
+++ src/main/java/edu/stanford/protege/webprotege/gateway/GatewayController.java
@@ -1,8 +1,12 @@
 package edu.stanford.protege.webprotege.gateway;
 
 import edu.stanford.protege.webprotege.common.UserId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
@@ -16,16 +20,15 @@ public class GatewayController {
 
     private final RpcRequestProcessor rpcRequestProcessor;
 
-
     public GatewayController(RpcRequestProcessor rpcRequestProcessor) {
         this.rpcRequestProcessor = rpcRequestProcessor;
     }
 
     @PostMapping(path = "/api/execute", consumes = "application/json")
     public Mono<RpcResponse> execute(@RequestBody RpcRequest request,
-                               @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
-        var accessToken = authorizedClient.getAccessToken().getTokenValue();
-        var userId = authorizedClient.getPrincipalName();
+                                     @AuthenticationPrincipal Jwt principal) {
+        var accessToken = principal.getTokenValue();
+        var userId = principal.getClaimAsString("preferred_username");
         var result = rpcRequestProcessor.processRequest(request, accessToken, new UserId(userId));
         return Mono.fromFuture(result);
     }
--- src/main/java/edu/stanford/protege/webprotege/gateway/SecurityConfig.java
+++ src/main/java/edu/stanford/protege/webprotege/gateway/SecurityConfig.java
@@ -1,8 +1,11 @@
 package edu.stanford.protege.webprotege.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 /**
  * Matthew Horridge
@@ -12,12 +15,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    String issuerUri;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .oauth2Login();
+        http.authorizeRequests(authz -> authz.antMatchers(HttpMethod.POST, "/api/execute")
+                                             .authenticated())
+            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
 }
