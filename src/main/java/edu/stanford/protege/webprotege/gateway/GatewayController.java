38fada6b4c8e45eea1f14811a5a460b219610f19
Matthew Horridge <matthew.horridge@stanford.edu>
2021-11-10 19:57:03 -0800

    Clean up

--- src/main/java/edu/stanford/protege/webprotege/gateway/GatewayController.java
+++ src/main/java/edu/stanford/protege/webprotege/gateway/GatewayController.java
@@ -1,16 +1,10 @@
 package edu.stanford.protege.webprotege.gateway;
 
 import edu.stanford.protege.webprotege.common.UserId;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
-
-import java.util.concurrent.ExecutionException;
 
 /**
  * Matthew Horridge
@@ -32,16 +26,6 @@ public class GatewayController {
         var accessToken = principal.getTokenValue();
         var userId = principal.getClaimAsString("preferred_username");
         var result = rpcRequestProcessor.processRequest(request, accessToken, new UserId(userId));
-        try {
-            result.get();
-            System.out.println(result);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
         return Mono.fromFuture(result);
     }
-
-
 }
