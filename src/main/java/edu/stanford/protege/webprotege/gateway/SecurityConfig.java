3d6cbda54bd1ad112627e6f02c48ebe8138b7e14
Matthew Horridge <matthew.horridge@stanford.edu>
2021-12-10 15:57:30 -0800

    Remove unused variable

--- src/main/java/edu/stanford/protege/webprotege/gateway/SecurityConfig.java
+++ src/main/java/edu/stanford/protege/webprotege/gateway/SecurityConfig.java
@@ -5,7 +5,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 /**
  * Matthew Horridge
@@ -14,14 +16,15 @@ import org.springframework.security.config.annotation.web.configurers.oauth2.ser
  */
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    String issuerUri;
-
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests(authz -> authz.antMatchers(HttpMethod.POST, "/api/execute")
-                                             .authenticated())
-            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        http.httpBasic()
+            .disable()
+            .formLogin(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeRequests(authz -> authz.antMatchers(HttpMethod.POST, "/api/execute").authenticated())
+            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+            .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
     }
 }
