f487ebd41ba7f126f29905f04d56396298008b6d
Matthew Horridge <matthew.horridge@stanford.edu>
2024-06-15 18:23:38 -0700

    Add endpoint for project forms import

--- src/main/java/edu/stanford/protege/webprotege/gateway/FormsController.java
+++ src/main/java/edu/stanford/protege/webprotege/gateway/FormsController.java
@@ -1,12 +1,16 @@
 package edu.stanford.protege.webprotege.gateway;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.stanford.protege.webprotege.common.*;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Map;
+import java.util.*;
 
 /**
  * Matthew Horridge
@@ -18,17 +22,43 @@ public class FormsController {
 
     private static final RpcMethod GET_FORM_DESCRIPTORS = new RpcMethod("webprotege.forms.GetProjectFormDescriptors");
 
+    private static final RpcMethod SET_FORMS = new RpcMethod("webprotege.forms.SetProjectForms");
+
+
     private static final String PROJECT_ID = "projectId";
 
     private final RpcClient rpcClient;
 
-    public FormsController(RpcClient rpcClient) {
+    private final ObjectMapper objectMapper;
+
+    public FormsController(RpcClient rpcClient, ObjectMapper objectMapper) {
         this.rpcClient = rpcClient;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping("/data/projects/{projectId}/forms")
     public ResponseEntity<Map<String, Object>> getForms(@PathVariable(PROJECT_ID) ProjectId projectId,
                                    @AuthenticationPrincipal Jwt jwt) {
         return rpcClient.call(jwt, GET_FORM_DESCRIPTORS, Map.of(PROJECT_ID, projectId));
+    }
+
+    @PostMapping("/data/projects/{projectId}/forms")
+    public ResponseEntity<Map<String, Object>> setForms(@PathVariable(PROJECT_ID) ProjectId projectId,
+                                         @RequestBody String forms,
+                                         @AuthenticationPrincipal Jwt jwt) {
+        // changeRequestId
+        // projectId,
+        // formDescriptors
+        // formSelectors
+        try {
+            var tree = objectMapper.readValue(forms, new TypeReference<Map<String, Object>>() {});
+            var params = new LinkedHashMap<>(tree);
+            params.put("changeRequestId", ChangeRequestId.generate());
+            params.put("projectId", projectId);
+            return rpcClient.call(jwt, SET_FORMS, params);
+
+        } catch (JsonProcessingException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 }
